d7acea7eef0e5736a95f033385263a471bf05744
Likilee <gnsdlrl@daum.net>
2022-06-28 21:19:44 +0900

    feat(client,App.tsx,index.tsx): 브라우저라우터 적용
    - App 에서 라우팅 기능을 적용하기위해 Index.tsx 에서 App 컴포넌트를 BrowserRouter 로 감싸주었습니다.
    - App.tsx 에서 루트 라우터가 DashBoard 컴포넌트를 호출하도록 설정했습니다.

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,8 +1,23 @@
+<<<<<<< HEAD
 import './App.css';
 import Board from './dashboard/presentation/components/Board/Board';
 
 function App() {
   return <Board />;
+=======
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import DashBoardPage from './dashboard/presentation/pages/DashBoardPage';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<DashBoardPage />} />
+      </Routes>
+    </div>
+  );
+>>>>>>> 2edffff (feat(client,App.tsx,index.tsx): 브라우저라우터 적용)
 }
 
 export default App;
