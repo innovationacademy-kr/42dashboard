5a01a93d11e6d2a3fb47628eb3897c3d6e08367b
doyun <flfns333@gmail.com>
2022-06-30 21:22:45 +0900

    fix(back-end DataSource 사용하는 곳): fixed: merge back-end in main
    기존에 playground에서 작업하던 코드를 42DashBoard에 합침.
     하지만, DataSouce 모듈을 사용하는 곳에서 의존성 문제 발생
    
    "feat #43", "re #69", "feat #93", "feat #115"

--- packages/server/src/config/key.ts
+++ packages/server/src/config/key.ts
@@ -1,9 +1,9 @@
 const app_api =
-  'https://api.intra.42.fr/oauth/authorize?client_id=b233a450fb76bb08837ba88ba9e0c568a22c3e42f4e48d9aa9f49e819e9a9916&redirect_uri=http%3A%2F%2Flocalhost%3A3000&response_type=code';
+  'https://api.intra.42.fr/oauth/authorize?42=b233a450fb76bb08837ba88ba9e0c568a22c3e42f4e48d9aa9f49e819e9a9916&redirect_uri=http%3A%2F%2Flocalhost%3A3000&response_type=code';
 const app_id =
-  'b233a450fb76bb08837ba88ba9e0c568a22c3e42f4e48d9aa9f49e819e9a9916';
+  'ea2e165515d03c90ddf5c01765248fa934fcbe34322b8b1c7dadac5628202128';
 const app_secret =
-  '23853995b5a49efdb7084b44c13b9963cfdaa4c9b1807c1db74da302cccfb2d2';
+  'f0ae7c076647decc9076ddfd0242b9a464ffb1316985a2fb23e886f1a85e1bac';
 const SPREAD_END_POINT2 = '1jjr1Nr1OTEoCmQhVdAM75LLg1V9ApxtLC8_Hb1Lfqvg';
 const SPREAD_END_POINT = '11plC3Jx-l8muXJCZgSqpB6LQsRld_MooxJthqMH4oKs';
 const SHEET_ID1 = '1538455769';
