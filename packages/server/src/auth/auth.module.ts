842e8f31442b3a16a373502d1f6ba0f1b7378588
huchoi <nyz1058@nate.com>
2022-07-07 14:19:52 +0900

    feat(server): 백엔드 42oauth 로그인 기능 구현
    42oauth 로그인 기능 구현하였습니다. 테스트해봤는데 잘 되는거 같습니다. 프론트 홈화면 나오면 그때 같이 테스트해봐요
    
    feat 31, feat 136

---
+++ packages/server/src/auth/auth.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { JwtStrategy } from './jwt.strategy';
+
+@Module({
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }), //기억해두기
+    JwtModule.register({
+      secret: 'huchoi',
+      signOptions: {
+        // expiresIn: 60 * 60,
+      },
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, JwtStrategy],
+  exports: [JwtStrategy, PassportModule],
+})
+export class AuthModule {}
---
+++ packages/server/src/auth/jwt.strategy.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { InjectDataSource } from '@nestjs/typeorm';
+import { userInfo } from 'os';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { DataSource } from 'typeorm';
+
+const cookieExtractor = function (req) {
+  let token: string = null;
+  if (req) token = String(req.headers.cookie);
+  const arr = token.split(';');
+  for (const idx in arr) {
+    const array = arr[idx].split('=');
+    array[0] = array[0].replace(/\s/g, ''); //white space제거
+    if (array[0] == 'access_token') {
+      return array[1]; //access_token 값을 반환
+    }
+  }
+  return null; //null을 반환
+};
+
+@Injectable() //이 strategy가 guard에서 쓰일 예정
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(@InjectDataSource() private dataSource: DataSource) {
+    super({
+      // 들어온 cookie와 payload를 통해 새로 만든 cookie를 비교해서 인가절차 진행
+      secretOrKey: 'huchoi',
+      jwtFromRequest: cookieExtractor,
+      // jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    });
+  }
+
+  async validate(payload) {
+    // const { email, login, first_name, last_name, usual_full_name } = payload;
+    const { id, login } = payload;
+    console.log(`${login} is login`);
+    return this.validateUser(payload);
+  }
+
+  async validateUser(payload) {
+    //DB에서 해당 사용자에 대한 정보가 있는지 체크 <- repo.findOne()하면 될듯?
+    if (payload) return payload;
+    else return null;
+  }
+}
