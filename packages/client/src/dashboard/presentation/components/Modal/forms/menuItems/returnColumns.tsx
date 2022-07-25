2f81fa0418cef0197c61c1369cb1c67a19385359
jinbekim <jinbekim@student.42seoul.kr>
2022-07-25 10:24:20 +0900

    refactor(client/modal): return menuitems on dependencies

---
+++ packages/client/src/dashboard/presentation/components/Modal/forms/menuItems/returnColumns.tsx
@@ -0,0 +1,19 @@
+import { MenuItem } from '@mui/material';
+import { EntityColumn } from 'common/src';
+
+type EntityColumnType = typeof EntityColumn;
+
+export default function returnColumns(dependencies: { [key: string]: string }) {
+  const { entityName } = dependencies;
+  return () => {
+    if (entityName)
+      return EntityColumn[entityName as keyof EntityColumnType].map(
+        (column: { spName: string; dbName: string }) => (
+          <MenuItem key={column.dbName} value={column.dbName}>
+            {column.spName}
+          </MenuItem>
+        ),
+      );
+    return null;
+  };
+}
