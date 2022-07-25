fe43436a5661dc09d38179f4e3af046be27c01bf
jinbekim <jinbekim@student.42seoul.kr>
2022-07-25 15:49:16 +0900

    fix(client): manage modal state one per section

--- packages/client/src/dashboard/presentation/components/Modal/Modal.tsx
+++ packages/client/src/dashboard/presentation/components/Modal/Modal.tsx
@@ -1,8 +1,6 @@
 import { Box, Modal } from '@mui/material';
 import { useState } from 'react';
-import useFiltersModal from '../../../application/services/useFiltersModal';
 import HorizontalLinearStepper from './Stepper';
-import { v4 as uuid } from 'uuid';
 import { QueryFilterType } from '../../../application/services/useDataset';
 import { StickerContentType } from '../Sticker/StickerContent.type';
 import StickerDataType from '../../../domain/stickerDatas/stickerData.type';
@@ -12,6 +10,8 @@ interface ModalProps {
   sectionId: string;
   func: (sectionId: string, stickerId: string) => void;
   addSticker: (sticker: StickerDataType) => Promise<void>;
+  isOpen: boolean;
+  setIsOpen: (isOpen: boolean) => void;
 }
 
 const style = {
@@ -28,8 +28,7 @@ const style = {
 };
 
 const ModalFrame = (props: ModalProps) => {
-  const { sectionId, func, addSticker } = props;
-  const { isOpen, cancelFiltersModal, applyFiltersModal } = useFiltersModal();
+  const { sectionId, func, addSticker, isOpen, setIsOpen } = props;
   const [type, setType] = useState<StickerContentType>('lineChart');
   const [labels, setLabels] = useState<string[]>([]);
   const [filters, setFilters] = useState<QueryFilterType[]>([]);
@@ -44,25 +43,17 @@ const ModalFrame = (props: ModalProps) => {
       arrayOfDataSet: dataSets,
     });
     /** turn off modal */
-    applyFiltersModal();
+    setIsOpen(false);
     /** store sticker data to store */
     addSticker(newStickerData);
     /** render sticker */
     func(sectionId, newStickerData.id);
-    const str = JSON.stringify(filters);
-    const data = JSON.stringify(dataSets);
-    console.log(
-      `***APPLY BUTTON*** Type: ${type} Labels: ${labels} Filters: ${str} dataSets: ${data})`,
-    );
-    setLabels((labels) => []);
-    setFilters((filters) => []);
-    setDataSets((dataSets) => [[]]);
   }
 
   return (
     <Modal
       open={isOpen}
-      onClose={cancelFiltersModal}
+      onClose={() => setIsOpen(false)}
       style={{
         position: 'relative',
         left: 'calc(50vw - 250px)',
