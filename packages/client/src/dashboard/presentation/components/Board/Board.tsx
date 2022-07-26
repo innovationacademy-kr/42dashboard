66796dddd6fba50b67ca75ccfbb75005aa2f5da0
jinbekim <jinbekim@student.42seoul.kr>
2022-07-26 18:24:34 +0900

    fix(client): save layout change when window resize event occur

--- packages/client/src/dashboard/presentation/components/Board/Board.tsx
+++ packages/client/src/dashboard/presentation/components/Board/Board.tsx
@@ -76,7 +76,7 @@ export default function Board() {
       <ReactGridLayout
         onDragStart={(a, b, c, d, e) => e.stopPropagation()}
         layouts={{ lg: boardData.sectionLayouts || [] }}
-        breakpoints={{ lg: 600, md: 498, sm: 384, xs: 240, xxs: 0 }}
+        breakpoints={{ lg: 600 }}
         cols={{ lg: 8, md: 5, sm: 4, xs: 2, xxs: 1 }}
         onLayoutChange={handleSectionLayoutChange}
         className="layout"
