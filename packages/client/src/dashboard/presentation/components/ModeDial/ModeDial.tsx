b556ff40a4630e9a38b255ace68d60f09531ca00
Hyeonsik Bae <baehyeonsik@gmail.com>
2022-07-26 16:59:18 +0900

    feat(ModeDial): feat: edit mode시, ModeDial 변경
    edit mode의 경우, 기존 ModeDial을 렌더하지 않고 view mode로 변환하는 버튼 렌더
    
    #289

--- packages/client/src/dashboard/presentation/components/ModeDial/ModeDial.tsx
+++ packages/client/src/dashboard/presentation/components/ModeDial/ModeDial.tsx
@@ -7,9 +7,28 @@ import IosShareIcon from '@mui/icons-material/IosShare';
 import FullscreenIcon from '@mui/icons-material/Fullscreen';
 import ModeEditIcon from '@mui/icons-material/ModeEdit';
 import useMode from '../../../application/services/useMode';
+import styled from '@emotion/styled';
 
 export default function ModeDial() {
-  const { setControlMode } = useMode();
+  const { setControlMode, getControlMode } = useMode();
+  const mode = getControlMode();
+
+  const RoundButton = styled.button`
+    position: relative;
+    top: 184px;
+    border-radius: 50%;
+    width: 56px;
+    height: 56px;
+    border: 0px;
+    background-color: #1976d2;
+    color: white;
+    cursor: pointer;
+    box-shadow: 0px 3px 5px -1px rgb(0 0 0 / 20%),
+      0px 6px 10px 0px rgb(0 0 0 / 14%), 0px 1px 18px 0px rgb(0 0 0 / 12%);
+    &:hover {
+      background-color: #1565c0;
+    }
+  `;
   return (
     <Box
       sx={{
@@ -21,29 +40,34 @@ export default function ModeDial() {
         bottom: 0,
       }}
     >
-      <SpeedDial ariaLabel="ModeDial" icon={<MenuIcon />}>
-        <SpeedDialAction
-          icon={<ModeEditIcon />}
-          tooltipTitle={'EditMode'}
-          onClick={() => {
-            setControlMode('edit');
-          }}
-        />
-        <SpeedDialAction
-          icon={<FullscreenIcon />}
-          tooltipTitle={'Fullscreen'}
-          onClick={() => {
-            setControlMode('fullscreen');
-          }}
-        />
-        <SpeedDialAction
-          icon={<IosShareIcon />}
-          tooltipTitle={'Export'}
-          onClick={() => {
-            setControlMode('export');
-          }}
-        />
-      </SpeedDial>
+      {mode === 'edit' && (
+        <RoundButton onClick={() => setControlMode('view')}>VIEW</RoundButton>
+      )}
+      {mode !== 'edit' && (
+        <SpeedDial ariaLabel="ModeDial" icon={<MenuIcon />}>
+          <SpeedDialAction
+            icon={<ModeEditIcon />}
+            tooltipTitle={'EditMode'}
+            onClick={() => {
+              setControlMode('edit');
+            }}
+          />
+          <SpeedDialAction
+            icon={<FullscreenIcon />}
+            tooltipTitle={'Fullscreen'}
+            onClick={() => {
+              setControlMode('fullscreen');
+            }}
+          />
+          <SpeedDialAction
+            icon={<IosShareIcon />}
+            tooltipTitle={'Export'}
+            onClick={() => {
+              setControlMode('export');
+            }}
+          />
+        </SpeedDial>
+      )}
     </Box>
   );
 }
