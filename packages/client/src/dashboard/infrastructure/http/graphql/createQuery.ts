9107bf725412185ee822c7acfd4badfb39dc7b8b
jinbekim <jinbekim@student.42seoul.kr>
2022-07-28 07:35:23 +0900

    docs(client): refac: createQuery func

--- packages/client/src/dashboard/infrastructure/http/graphql/createQuery.ts
+++ packages/client/src/dashboard/infrastructure/http/graphql/createQuery.ts
@@ -1,26 +1,73 @@
 import { gql } from '@apollo/client';
 
+/**
+ *
+ * @param filterNames
+ * @returns
+ * [$nameOfFilter1: Filter!, $nameOfFilter2: Filter!, $nameOfFilter3: Filter!]
+ */
+function returnFilterVariables(filterNames: string[]): string[] {
+  const arrayOfFilterName = filterNames.map(
+    (filterName): string => `$${filterName}: Filter!`,
+  );
+  return arrayOfFilterName;
+}
+
+/**
+ * @param filterSet
+ * @returns
+ * `$nameOfFilter1, $nameOfFilter1, $nameOfFilter2`
+ */
+function returnFilters(filterSet: string[]): string {
+  const filters = filterSet.map((filterName): string => `$${filterName}`);
+  return filters.join(', ');
+}
+
+/**
+ *
+ * @param index
+ * @returns
+ * alias
+ */
+function returnAlias(labels: string[], index: number): string {
+  const serialNumber = Math.floor(index / labels.length);
+  return `a${index}${serialNumber}`;
+}
+
+/**
+ * sometin: getNumOfPeopleByFilter(filters: [$filtersGrade, $filtersMan])
+ */
+function returnRequest(labels: string[]) {
+  return (filterSet: string[], index: number): string => {
+    const alias = returnAlias(labels, index);
+    const queryName = 'getNumOfPeopleByFilter';
+    const filters = returnFilters(filterSet);
+    return `${alias}: ${queryName}(filters: [${filters}])`;
+  };
+}
+
+/**
+ * @param filterNames
+ * @param labels
+ * @param filterSetsPerData
+ * @returns
+ * `query queryName(
+ * $filtersGrade: Filter!
+ * ) {
+ *   alias: getNumOfPeopleByFilter(filters: [$filtersGrade])
+ * }`
+ */
 export default function createQuery(
   filterNames: string[],
   labels: string[],
   filterSetsPerData: string[][],
 ) {
-  const result = `
-  query GetDatasets(
-    ${filterNames.map((filterName) => `$${filterName}: Filter!`).join('\n')}
+  //TODO : error handlers
+
+  const query = `query GetDatasets(
+    ${returnFilterVariables(filterNames).join('\n')}
   ) {
-    ${filterSetsPerData
-      .map((filterSet, index) => {
-        const alias = `${labels[index % labels.length]}${Math.floor(
-          index / labels.length,
-        )}`;
-        const queryName = `getNumOfPeopleByFilter`;
-        const filters = `filters: [${filterSet
-          .map((filterName) => `$${filterName}`)
-          .join(', ')}]`;
-        return `${alias}: ${queryName}(${filters})`;
-      })
-      .join('\n')}
-    }`;
-  return gql(result);
+    ${filterSetsPerData.map(returnRequest(labels)).join('\n')}
+  }`;
+  return gql(query);
 }
