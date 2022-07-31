d11938997c6a6d0fe05c1442cae35d472ef71e7b
Hyeonsik Bae <baehyeonsik@gmail.com>
2022-07-31 22:42:01 +0900

    feat(component): feat: add Text component
    Sticker의 Contents 중 하나인 Text component 생성
    
    #287

---
+++ packages/client/src/dashboard/presentation/components/Text/Text.tsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import styled from '@emotion/styled';
+import useMode from '../../../application/services/useMode';
+
+interface TextProps {
+  content?: string;
+}
+
+const TextContent = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  padding: 0.5rem;
+  background-color: lightgrey;
+`;
+
+const TextEdit = styled.input`
+  width: 100%;
+  height: 100%;
+`;
+
+const Text = (props: TextProps) => {
+  const { content } = props;
+  const [text, setText] = useState(content);
+  const { getControlMode } = useMode();
+
+  const handleTextEdit = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+  };
+
+  return (
+    <TextContent>
+      {getControlMode() === 'edit' ? (
+        <TextEdit onChange={handleTextEdit} value={text} />
+      ) : (
+        <span>{text || 'EMPTY'}</span>
+      )}
+    </TextContent>
+  );
+};
+
+export default Text;
