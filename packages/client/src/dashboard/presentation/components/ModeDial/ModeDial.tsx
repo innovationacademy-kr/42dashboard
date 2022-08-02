5fbb393041b64df6e787d6c50e71d48d326dd4f1
Hyeonsik Bae <baehyeonsik@gmail.com>
2022-08-02 18:37:26 +0900

    feat(board): feat: add data refresh button
    add data refresh button
    
    #344

--- packages/client/src/dashboard/presentation/components/ModeDial/ModeDial.tsx
+++ packages/client/src/dashboard/presentation/components/ModeDial/ModeDial.tsx
@@ -6,29 +6,51 @@ import SpeedDialAction from '@mui/material/SpeedDialAction';
 import IosShareIcon from '@mui/icons-material/IosShare';
 import FullscreenIcon from '@mui/icons-material/Fullscreen';
 import ModeEditIcon from '@mui/icons-material/ModeEdit';
+import RefreshIcon from '@mui/icons-material/Refresh';
 import useMode from '../../../application/services/useMode';
 import styled from '@emotion/styled';
+import { gql, NetworkStatus, useLazyQuery, useQuery } from '@apollo/client';
+import { keyframes } from '@emotion/react';
+import * as axios from './../../../infrastructure/http/axios/axios.custom';
+
+const RoundButton = styled.button`
+  position: relative;
+  top: 240px;
+  border-radius: 50%;
+  width: 56px;
+  height: 56px;
+  border: 0px;
+  background-color: #1976d2;
+  color: white;
+  cursor: pointer;
+  box-shadow: 0px 3px 5px -1px rgb(0 0 0 / 20%),
+    0px 6px 10px 0px rgb(0 0 0 / 14%), 0px 1px 18px 0px rgb(0 0 0 / 12%);
+  &:hover {
+    background-color: #1565c0;
+  }
+`;
+
+const rotation = keyframes`
+from {
+  transform: rotate(0deg);
+}
+to {
+  transform: rotate(360deg);
+}`;
+
+const RotateRefreshIcon = styled(RefreshIcon)`
+  &.loading {
+    animation: ${rotation} 1s linear infinite;
+    transform-origin: 50% 50%;
+  }
+`;
 
 export default function ModeDial() {
   const { setControlMode, getControlMode } = useMode();
+  const [refetchState, setRefetchState] = React.useState('data');
+
   const mode = getControlMode();
 
-  const RoundButton = styled.button`
-    position: relative;
-    top: 184px;
-    border-radius: 50%;
-    width: 56px;
-    height: 56px;
-    border: 0px;
-    background-color: #1976d2;
-    color: white;
-    cursor: pointer;
-    box-shadow: 0px 3px 5px -1px rgb(0 0 0 / 20%),
-      0px 6px 10px 0px rgb(0 0 0 / 14%), 0px 1px 18px 0px rgb(0 0 0 / 12%);
-    &:hover {
-      background-color: #1565c0;
-    }
-  `;
   return (
     <Box
       sx={{
@@ -37,7 +59,7 @@ export default function ModeDial() {
         flexGrow: 1,
         position: 'fixed',
         right: 25,
-        bottom: 0,
+        bottom: 56,
       }}
     >
       {mode === 'edit' && (
@@ -64,6 +86,24 @@ export default function ModeDial() {
             tooltipTitle={'Export'}
             onClick={() => {
               setControlMode('export');
+            }}
+          />
+          <SpeedDialAction
+            icon={<RotateRefreshIcon className={refetchState} />}
+            tooltipTitle={'Refresh'}
+            onClick={() => {
+              setRefetchState('loading');
+              console.log(`loading`);
+              axios
+                .axiosUpdateData()
+                .then((data) => {
+                  setRefetchState('data');
+                  console.log(`data`);
+                })
+                .catch((error) => {
+                  setRefetchState('error');
+                  console.log(`error`);
+                });
             }}
           />
         </SpeedDial>
