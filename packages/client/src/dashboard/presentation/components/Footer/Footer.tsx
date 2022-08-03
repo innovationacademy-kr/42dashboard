44e44227105aa909e51b01e41db948945e208bfb
Hyeonsik Bae <baehyeonsik@gmail.com>
2022-08-03 20:31:39 +0900

    feat(footer): feat: add footer instead of mode dial
    Mode Dial을 Footer로 변경
    
    #345

---
+++ packages/client/src/dashboard/presentation/components/Footer/Footer.tsx
@@ -0,0 +1,108 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import { keyframes } from '@emotion/react';
+import * as axios from './../../../infrastructure/http/axios/axios.custom';
+import useMode from '../../../application/services/useMode';
+import FullscreenIcon from '@mui/icons-material/Fullscreen';
+import ModeEditIcon from '@mui/icons-material/ModeEdit';
+import RefreshIcon from '@mui/icons-material/Refresh';
+import IosShareIcon from '@mui/icons-material/IosShare';
+
+const RoundButton = styled.button`
+  display: inline-block;
+  justify-content: center;
+  align-item: center;
+  border-radius: 50%;
+  width: 40px;
+  height: 40px;
+  border: 0px;
+  margin: 0.5rem;
+  padding: 0;
+  background-color: #1976d2;
+  color: white;
+  cursor: pointer;
+  box-shadow: 0px 3px 5px -1px rgb(0 0 0 / 20%),
+    0px 6px 10px 0px rgb(0 0 0 / 14%), 0px 1px 18px 0px rgb(0 0 0 / 12%);
+  &:hover {
+    background-color: #1565c0;
+  }
+  font-size: 0.7rem;
+`;
+
+const rotation = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const RotateRefreshIcon = styled(RefreshIcon)`
+  &.loading {
+    animation: ${rotation} 1s linear infinite;
+    transform-origin: 50% 50%;
+  }
+`;
+
+const FooterArea = styled.footer`
+  position: fixed;
+  width: calc(100% - 240px);
+  bottom: 0;
+  background-color: #1976d2;
+`;
+
+const ButtonArea = styled.div`
+  position: relative;
+  float: right;
+`;
+
+function Footer() {
+  const { setControlMode, getControlMode } = useMode();
+  const [refetchState, setRefetchState] = React.useState('data');
+
+  const mode = getControlMode();
+
+  return (
+    <FooterArea>
+      {mode === 'edit' && (
+        <ButtonArea>
+          <RoundButton onClick={() => setControlMode('view')}>VIEW</RoundButton>
+        </ButtonArea>
+      )}
+      {mode !== 'edit' && (
+        <ButtonArea>
+          <RoundButton
+            onClick={() => {
+              setRefetchState('loading');
+              console.log(`loading`);
+              axios
+                .axiosUpdateData()
+                .then((data) => {
+                  setRefetchState('data');
+                  console.log(`data`);
+                })
+                .catch((error) => {
+                  setRefetchState('error');
+                  console.log(`error`);
+                });
+            }}
+          >
+            <RotateRefreshIcon className={refetchState} />
+          </RoundButton>
+          <RoundButton onClick={() => setControlMode('fullscreen')}>
+            <FullscreenIcon />
+          </RoundButton>
+          <RoundButton onClick={() => setControlMode('export')}>
+            <IosShareIcon />
+          </RoundButton>
+          <RoundButton title="edit" onClick={() => setControlMode('edit')}>
+            <ModeEditIcon />
+          </RoundButton>
+        </ButtonArea>
+      )}
+    </FooterArea>
+  );
+}
+
+export default Footer;
--- packages/client/src/dashboard/presentation/components/MainArea/MainArea.tsx
+++ packages/client/src/dashboard/presentation/components/MainArea/MainArea.tsx
@@ -1,12 +1,14 @@
 import { Toolbar } from '@mui/material';
 import { Box } from '@mui/system';
 import { ReactNode } from 'react';
+import Footer from '../Footer/Footer';
+
 export interface MainAreaProps {
   children: ReactNode;
 }
 function MainArea(props: MainAreaProps) {
   return (
-    <Box component="main" sx={{ flexGrow: 1, p: 2 }}>
+    <Box component="main" sx={{ flexGrow: 1 }}>
       <Toolbar />
       {props.children}
     </Box>
