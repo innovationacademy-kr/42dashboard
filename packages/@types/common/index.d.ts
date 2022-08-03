00b94cf452e0d982b5e99501f7e2491a2a5c6144
한중명 <gkswndaud@gmail.com>
2022-08-03 20:18:01 +0900

    feat(common): feat: add table name
    테이블 이름을 저장하는 변수를 만듦

--- packages/@types/common/index.d.ts
+++ packages/@types/common/index.d.ts
@@ -3,3 +3,7 @@ export const Operator: { [key: string]: string };
 export const EntityColumn: {
   [key: string]: { nbName: string; dbName: string }[];
 };
+
+export const tableName: {
+  [key: string]: string;
+};
