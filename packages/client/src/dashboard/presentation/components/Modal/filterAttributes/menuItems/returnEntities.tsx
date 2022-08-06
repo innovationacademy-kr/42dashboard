0ce072a114e06ef520c40777ab7795a0fe01e35f
jinbekim <jinbekim@student.42seoul.kr>
2022-08-06 21:32:56 +0900

    fix(client): modal entity name to hangeul

--- packages/client/src/dashboard/presentation/components/Modal/filterAttributes/menuItems/returnEntities.tsx
+++ packages/client/src/dashboard/presentation/components/Modal/filterAttributes/menuItems/returnEntities.tsx
@@ -1,11 +1,12 @@
-import { EntityColumn } from 'common/src';
+import { EntityColumn, tableName } from 'common/src';
 import { MenuItem } from '@mui/material';
+import camelize from '../../../../../application/utils/camelize';
 
 export default function returnEntities() {
   return () =>
     Object.keys(EntityColumn).map((entity) => (
       <MenuItem key={entity} value={entity}>
-        {entity}
+        {tableName[camelize(entity) as keyof typeof tableName]}
       </MenuItem>
     ));
 }
