d75e81a09fe173b811c7c6ad5538e327b5c5a0f2
jinbekim <jinbekim@student.42seoul.kr>
2022-08-08 17:14:08 +0900

    feat(client): edit label

--- packages/client/src/dashboard/presentation/components/Modal/SelectedFilter.tsx
+++ packages/client/src/dashboard/presentation/components/Modal/SelectedFilter.tsx
@@ -1,11 +1,16 @@
-import { Button, ListItem, ListItemText } from '@mui/material';
+import { Button, ListItem, ListItemText, TextField } from '@mui/material';
 import { tableName } from 'common/src';
+import { useState } from 'react';
 import camelize from '../../../application/utils/camelize';
 import { SelectedLabelFilters } from './InputLabels';
 
 interface PropType {
   idx: number;
   data: SelectedLabelFilters;
+  setLabels?: React.Dispatch<React.SetStateAction<string[]>>;
+  setSelectedLabels?: React.Dispatch<
+    React.SetStateAction<SelectedLabelFilters[]>
+  >;
   removeFilter: (idx: number) => void;
 }
 
@@ -14,15 +19,61 @@ export default function SelectedFilter(props: PropType) {
     idx,
     data: { label, latest, entityName, column, givenValue, operator },
     removeFilter,
+    setLabels,
+    setSelectedLabels,
   } = props;
-  return (
-    <ListItem key={idx}>
+  const [editmode, setEditmode] = useState(false);
+  const [value, setValue] = useState(label);
+
+  function FilterText() {
+    return (
       <ListItemText
         primary={label}
         secondary={`${
           tableName[camelize(entityName) as keyof typeof tableName]
         }의 ${column}이 ${givenValue}고 operator는 ${operator}이고 latest는 ${latest}입니다.`}
       />
+    );
+  }
+
+  function EditText() {
+    return (
+      <TextField
+        autoFocus={true}
+        variant="standard"
+        placeholder={value}
+        value={value}
+        onChange={EditLabel}
+      />
+    );
+  }
+
+  function EditLabel(e: any) {
+    setValue(e.target.value);
+  }
+
+  function onClick() {
+    setEditmode((prev) => {
+      if (prev && setLabels && value && setSelectedLabels) {
+        setLabels((prevLabels) => {
+          const newLabels = [...prevLabels];
+          newLabels[idx] = value;
+          return newLabels;
+        });
+        setSelectedLabels((prevLabels) => {
+          const newLabels = [...prevLabels];
+          newLabels[idx].label = value;
+          return newLabels;
+        });
+      }
+      return !prev;
+    });
+  }
+
+  return (
+    <ListItem key={idx}>
+      {editmode ? <EditText /> : <FilterText />}
+      {setLabels && <Button onClick={onClick}>수정</Button>}
       <Button onClick={() => removeFilter(idx)}>delete</Button>
     </ListItem>
   );
