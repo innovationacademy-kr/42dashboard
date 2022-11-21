c849431caa4f76b6aa5f322983745d2d3223ef33
xhseb <zzzu5280@gmail.com>
2022-11-21 15:17:49 +0900

    feat(client): axios로 401 error받았을 때 login 페이지로 라우팅
    #523

--- packages/client/src/dashboard/infrastructure/http/axios/axios.instance.ts
+++ packages/client/src/dashboard/infrastructure/http/axios/axios.instance.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import useUser from '../../../application/services/useUser';
 
 export const instance = axios.create({
   baseURL: `${process.env.REACT_APP_API_URI}`,
@@ -8,3 +9,23 @@ export const instance = axios.create({
     return status >= 200 && status < 300;
   },
 });
+
+const Interceptor = ({ children }: any) => {
+  const { setUser } = useUser();
+
+  instance.interceptors.response.use(
+    function (response) {
+      return response;
+    },
+    async function (error) {
+      console.log(error.response.status);
+      if (error.response?.status === 401) {
+        await setUser(null);
+        window.location.href = '/';
+      }
+    },
+  );
+  return children;
+};
+
+export { Interceptor };
--- packages/client/src/dashboard/infrastructure/preset.repository.ts
+++ packages/client/src/dashboard/infrastructure/preset.repository.ts
@@ -1,21 +1,19 @@
 import PresetRepositoryInterface from '../domain/preset/preset.repository.interface';
 import PresetType from '../domain/preset/preset.type';
 import presetStore from './store/preset.store';
-import axios from 'axios';
-
+import {
+  axiosGetOnePreset,
+  axiosGetAllPreset,
+  axiosUpdatePreset,
+  axiosAddPreset,
+  axiosDeletePreset,
+} from './http/axios/axios.custom';
 class PresetRepository implements PresetRepositoryInterface {
   public async getPreset(id: string): Promise<PresetType | null> {
-    const preset = await axios
-      .get(
-        `${process.env.REACT_APP_API_URI}${process.env.REACT_APP_GET_ONE_PRESET}/${id}`,
-        {
-          withCredentials: true,
-        },
-      )
-      .then((res) => {
-        console.log('get result from db', res);
-        return res.data[0];
-      });
+    const preset = await axiosGetOnePreset(id).then((res) => {
+      console.log('get result from db', res);
+      return res.data[0];
+    });
     return preset;
   }
 
@@ -26,49 +24,21 @@ class PresetRepository implements PresetRepositoryInterface {
   public async savePreset(preset: PresetType): Promise<void> {
     const { id } = preset;
     let isExist = false;
-    await axios
-      .get(
-        `${process.env.REACT_APP_API_URI}${process.env.REACT_APP_GET_ALL_PRESET}`,
-        {
-          withCredentials: true,
-        },
-      )
-      .then((res) => {
-        for (let i = 0; i < res.data.length; i++) {
-          if (res.data[i].id === id) isExist = true;
-        }
-      });
+    await axiosGetAllPreset().then((res) => {
+      for (let i = 0; i < res.data.length; i++) {
+        if (res.data[i].id === id) isExist = true;
+      }
+    });
     if (isExist) {
-      await axios.put(
-        `${process.env.REACT_APP_API_URI}${process.env.REACT_APP_UPDATE_ONE_PRESET}/${id}`,
-        preset,
-        {
-          withCredentials: true,
-        },
-      );
+      await axiosUpdatePreset(id, preset);
     }
     if (!isExist) {
-      await axios.post(
-        `${process.env.REACT_APP_API_URI}${process.env.REACT_APP_ADD_PRESET}`,
-        preset,
-        {
-          withCredentials: true,
-        },
-      );
+      await axiosAddPreset(preset);
     }
   }
 
   public async deletePreset(id: string): Promise<void> {
-    await axios
-      .delete(
-        `${process.env.REACT_APP_API_URI}${process.env.REACT_APP_DELETE_ONE_PRESET}/${id}`,
-        {
-          withCredentials: true,
-        },
-      )
-      .then((res) => {
-        console.log('delete result: from db ', res);
-      });
+    await axiosDeletePreset(id);
   }
 }
 
